ed62cb6e6e244fbe1f3edde0312fa78e8bb95507
Dave Abrahams <dabrahams@adobe.com>
2022-01-06 19:55:52 -0800

    Restore centering in reveal config; it is needed for auto-animate

--- js/reveal-config.js
+++ js/reveal-config.js
@@ -3,6 +3,10 @@ export default {
   transition: 'fade',
   backgroundTransition: 'fade',
   transitionSpeed: 'slow',
+  // We use absolute positioning, which is apparently overridden by
+  // “center” until transitions finish, so this needs to be disabled to
+  // avoid weird effects when data-auto-animate is set.
+  center: false,
   // At the bottom of slides, especially, a little more room is needed
   // than the default 0.04. We could expand just the bottom in CSS.
   margin: 0
